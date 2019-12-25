8bc2eaaaeb222461751ab3940b3ec880bd604968
unknown <warun.ser@gmail.com>
2019-12-25 15:06:51 +0700

    My first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Button,ButtonToolbar} from 'react-bootstrap';
+import Header from './Header';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="container">
+        <Header />
+        <ButtonToolbar>
+          <Button variant="outline-primary">Primary</Button>
+          <Button variant="outline-secondary">Secondary</Button>
+          <Button variant="outline-success">Success</Button>
+          <Button variant="outline-warning">Warning</Button>
+          <Button variant="outline-danger">Danger</Button>
+          <Button variant="outline-info">Info</Button>
+          <Button variant="outline-light">Light</Button>
+          <Button variant="outline-dark">Dark</Button>
+        </ButtonToolbar>
+       
+      
+    </div>
+  );
+}
+
+export default App;
